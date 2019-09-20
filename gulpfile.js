ac1c64c2b2c686cc12d0a267cc3ed1fe0a1df705
alex-lys <la.prof@ya.ru>
2019-09-20 04:43:04 +0300

    add fonts, add new scss blocks

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/scss/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
@@ -30,7 +30,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+    gulp.watch("src/scss/**/*.+(scss|sass|css)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
     gulp.watch("src/*.html").on("change", gulp.parallel('html'));
 });
